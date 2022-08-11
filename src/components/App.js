17529a7abbc13a293785bcf98afba2c0705191c1
singharpit0911@gmail.com <singharpit0911@gmail.com>
2022-08-11 10:44:37 +0530

    class name change in name and percent

--- src/components/App.js
+++ src/components/App.js
@@ -49,10 +49,10 @@ class App extends Component {
                 // if the percent >= 90 else className = ""
                 <div
                   key={index}
-                  className={Math.round(item.percent) >= 90 ? "bg-pink" : ""}
+                  className={item.percent >= 90 ? "bg-pink" : ""}
                 >
-                  <div className="name">{item.percent.toFixed(2)}</div>
-                  <div className="percent">{item.name}</div>
+                  <div className="name">{item.name}</div>
+                  <div className="percent">{item.percent.toFixed(2)}</div>
                 </div>
               );
             }
